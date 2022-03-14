6e8fc846061c936f0730e12f373f9d325b820ee0
galebalis <galebjalis@gmail.com>
2022-03-13 21:16:23 -0400

    unidad2

---
+++ main.js
@@ -0,0 +1,21 @@
+let cantidadDeVentanas = 12;
+console.log(cantidadDeVentanas);
+
+let soyHumano = true;
+console.log(soyHumano);
+
+let miGustoDePizzaPreferido = "mozzarella";
+console.log(miGustoDePizzaPreferido);
+console.log(
+  "la cantidad de ventanas de mi Apto es de " +
+    cantidadDeVentanas +
+    " ventanas, ",
+  "Y SI Soy HUMANO;" + soyHumano,
+  "y mi gusto preferido de la pizza es " + miGustoDePizzaPreferido
+);
+
+let mismaVariable = "hola";
+mismaVariable = 0;
+mismaVariable = "minions";
+
+console.log(mismaVariable);
